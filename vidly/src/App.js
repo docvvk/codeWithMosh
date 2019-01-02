9295fc6364f93ab682ee0a7e9f91feb9d9f48355
Vivek Malhan <vivek.malhan@yahoo.com>
2019-01-02 12:10:12 +0530

    routing

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -4,19 +4,23 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from './components/movieForm';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <NavBar />
         <Switch>
+          <Route path='/movies/:id' component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
           <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies"></Redirect>
-          <Redirect to='/not-found'></Redirect>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
         </Switch>
       </main>
     );
