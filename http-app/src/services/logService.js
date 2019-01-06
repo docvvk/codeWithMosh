973f0d53cfdb82c0be48dde4a305a8db4554eba2
Vivek Malhan <vivek.malhan@yahoo.com>
2019-01-07 00:55:40 +0530

    adding http-backend folder

---
+++ http-app/src/services/logService.js
@@ -0,0 +1,17 @@
+import Raven from "raven-js";
+
+function init() {
+  Raven.config("https://24e617a215cf45cc84851867531bfde0@sentry.io/1365235", {
+    release: "1-0-0",
+    environment: "development-test"
+  }).install();
+}
+
+function log(error) {
+  Raven.captureException(error);
+}
+
+export default {
+  init,
+  log
+};
