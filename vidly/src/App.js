d723a8feca1da403b173d2ed0399f71f81b104f9
Vivek Malhan <vivek.malhan@yahoo.com>
2019-01-07 23:55:37 +0530

    backend vidly completed

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
-import MovieForm from './components/movieForm';
-import LoginForm from './components/loginForm';
-import RegisterForm from './components/registerForm';
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <ToastContainer />
         <NavBar />
         <Switch>
-          <Route path='/register' component={RegisterForm} />
-          <Route path='/login' component={LoginForm} />
-          <Route path='/movies/:id' component={MovieForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
